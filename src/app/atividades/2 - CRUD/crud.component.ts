6db181e67906eb11bd3bf7e150ef83a8fe51f306
MirandaOtavio <44755868+OtavioManoel@users.noreply.github.com>
2024-09-18 12:14:07 -0300

    Adiciona filtro de busca no items da grid
    - Implementa debouceTime, distinctUntilChanged e takeUntil

--- src/app/atividades/2 - CRUD/crud.component.ts
+++ src/app/atividades/2 - CRUD/crud.component.ts
@@ -1,39 +1,52 @@
-import { Component, OnInit } from '@angular/core';
-import { MatDialog } from "@angular/material/dialog"
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { FormularioComponent } from './formulario/formulario.component';
 import { asLiteral } from '@angular/compiler/src/render3/view/util';
 import { FormControl } from '@angular/forms';
+import { Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-crud',
   templateUrl: './crud.component.html',
   styleUrls: ['./crud.component.scss']
 })
-export class CrudComponent implements OnInit {
-  
+export class CrudComponent implements OnInit, OnDestroy {
+
   constructor(private dialog: MatDialog) { }
   
-  filtro = new FormControl()
+  filtro = new FormControl('')
+
+  private unsubscribe$ = new Subject<void>();
 
   displayedColumns: string[] = ['actions', 'nome', 'email', 'senha', 'cep', 'logradouro'];
   dataSource: Pessoa[] = [
-    new Pessoa("Teste1", "teste@email1.com", "1234", "80250104", "Rua teste")
-  ]
+    { nome: 'Teste1', email: 'teste@email1.com', senha: '1234', cep: '80250104', logradouro: 'Rua teste' }
+  ];
+
+  filteredDataSource: Pessoa[] = [...this.dataSource];
 
   ngOnInit(): void {
-    this.filtro.valueChanges.subscribe(valor => {
-      this.filtrar(valor)
+    this.filtro.valueChanges.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      takeUntil(this.unsubscribe$)
+    ).subscribe(valor => {
+      this.filtrar(valor);
     })
   }
 
-  filtrar(arg: string) {
-    console.log("filtrando...") //não remover essa linha
+  filtrar(valor: string) {
+    const filtro = valor.toLowerCase();
+    this.filteredDataSource = this.dataSource.filter(item =>
+      item.nome.toLowerCase().includes(filtro)
+    );
   }
 
   adicionar() {
     const dialogRef = this.dialog.open(FormularioComponent)
 
-    dialogRef.componentInstance.formularioSubmetido.subscribe(dados => {
+    dialogRef.componentInstance.formularioSubmetido.subscribe((dados: PessoaFormData) => {
       const novaPessoa = new Pessoa(
         dados.nome,
         dados.email,
@@ -42,7 +55,7 @@ export class CrudComponent implements OnInit {
         dados.logradouro
       )
       this.dataSource.push(novaPessoa)
-      this.dataSource = [...this.dataSource]
+      this.filteredDataSource = [...this.dataSource]
     });
   }
 
@@ -52,9 +65,15 @@ export class CrudComponent implements OnInit {
 
   remover(pessoa: Pessoa) {
     if (!confirm(`Deseja remover a pessoa ${pessoa.nome}?`)) return
-    
+
     this.dataSource = this.dataSource.filter(p => p !== pessoa)
+    this.filteredDataSource = [...this.dataSource]
     alert("removido com sucesso!")
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 }
 
@@ -69,3 +88,10 @@ class Pessoa {
   ) {}
 }
 
+interface PessoaFormData {
+  nome: string;
+  email: string;
+  senha: string;
+  cep: string;
+  logradouro: string;
+}
